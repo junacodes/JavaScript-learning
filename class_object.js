26620cc95a0ccdef7f9c4b414479f1ed58502ea3
junacodes <junacodes@gmail.com>
2024-06-26 22:06:52 +0545

    commit

--- class_object.js
+++ class_object.js
@@ -14,34 +14,67 @@
 // // console.log(person1.age);
 // person1.greet();
 
-class Book  {
-    constructor(title, author, pages){
-this.title = title,
-this.author = author,
-this.pages = pages
+// class Book  {
+//     constructor(title, author, pages){
+// this.title = title,
+// this.author = author,
+// this.pages = pages
 
-}
-getTitleBook(){
-    console.log(`title : ${this.title},  author: ${this.author}, pages :  ${this.pages} `)
-}
+// }
+// getTitleBook(){
+//     console.log(`title : ${this.title},  author: ${this.author}, pages :  ${this.pages} `)
+// }
 
-}
-// let book1 = new book("Alice's Adventures in Wonderland", "Lewis Carroll", 352)
-// // console.log(book1.title);
-// // console.log(book1.author);
-// // console.log(book1.pages);
+// }
+// // let book1 = new book("Alice's Adventures in Wonderland", "Lewis Carroll", 352)
+// // // console.log(book1.title);
+// // // console.log(book1.author);
+// // // console.log(book1.pages);
 
 
-class EBook extends Book{
-    constructor(title, author, pages, file_size){
-        super(title, author, pages);
-        this.file_size = file_size;
+// class EBook extends Book{
+//     constructor(title, author, pages, file_size){
+//         super(title, author, pages);
+//         this.file_size = file_size;
+//     }
+//     getTitleBook(){
+//         console.log(`title: ${this.title} ,author: ${this.author}, page:${this.pages} File size: ${this.file_size}`)
+//     }
+// } 
+// const book = new Book ("Alice's Adventures in Wonderland", "Lewis Carroll", 352)
+// const ebook = new EBook("Alice's Adventures in Wonderland", "Lewis Carroll", 352, 2)
+// console.log(book.getTitleBook())
+// console.log(ebook.getTitleBook()); 
+
+
+// Q2
+class Shape {
+    calculatorArea (){
+        return 0;
     }
-    getTitleBook(){
-        console.log(`title: ${this.title} ,author: ${this.author}, page:${this.pages} File size: ${this.file_size}`)
+}
+class Circle extends Shape{
+    constructor(radius){
+        super();
+        this.radius = radius;
     }
-} 
-const book = new Book ("Alice's Adventures in Wonderland", "Lewis Carroll", 352)
-const ebook = new EBook("Alice's Adventures in Wonderland", "Lewis Carroll", 352, 2)
-console.log(book.getTitleBook())
-console.log(ebook.getTitleBook()); 
+    calculatorArea(){
+        return Math.PI * this.radius ** 2
+    }
+}
+class Rectangle extends Shape{
+    constructor(width,height){
+        super();
+        this.width = width;
+        this.height = height;
+
+    }
+    calculatorArea(){
+        return this.width * this.height;
+    }
+
+}
+const circle = new Circle (6);
+console.log(`Area of circle : ${circle.calculatorArea()}`);
+const rectangle = new Rectangle(12 ,35);
+console.log(`Area of rectangle : ${rectangle.calculatorArea()}`)
