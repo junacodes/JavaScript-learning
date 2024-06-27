d601ae018d236abb7f2341edba87f836c3be720c
junacodes <junacodes@gmail.com>
2024-06-27 12:08:41 +0545

    commit

--- class_object.js
+++ class_object.js
@@ -48,33 +48,108 @@
 
 
 // Q2
-class Shape {
-    calculatorArea (){
-        return 0;
+// class Shape {
+//     calculatorArea (){
+//         return 0;
+//     }
+// }
+// class Circle extends Shape{
+//     constructor(radius){
+//         super();
+//         this.radius = radius;
+//     }
+//     calculatorArea(){
+//         return Math.PI * this.radius ** 2
+//     }
+// }
+// class Rectangle extends Shape{
+//     constructor(width,height){
+//         super();
+//         this.width = width;
+//         this.height = height;
+
+//     }
+//     calculatorArea(){
+//         return this.width * this.height;
+//     }
+
+// }
+// const circle = new Circle (6);
+// console.log(`Area of circle : ${circle.calculatorArea()}`);
+// const rectangle = new Rectangle(12 ,35);
+// console.log(`Area of rectangle : ${rectangle.calculatorArea()}`)
+
+
+// class Book {
+//     static count = 0 ;
+//     constructor (title , author ){
+//         this.title = title;
+//         this.author = author;
+//         Book.count++
+//     }
+//     static  getBookCount(){
+//         return Book.count;
+//     }
+// }
+// const book1 = new Book('1990', 'George Orwell')
+// const book2 = new Book('To Kill a Mockingbird', 'Harper Lee');
+// console.log(Book.getBookCount());
+
+// Q3
+class operation{
+    static addNumbers(x, y ){
+        return x + y 
     }
+    static multiplyNumbers(x ,y){
+        return x * y ; 
+    }
+
 }
-class Circle extends Shape{
-    constructor(radius){
-        super();
-        this.radius = radius;
-    }
-    calculatorArea(){
-        return Math.PI * this.radius ** 2
-    }
-}
-class Rectangle extends Shape{
-    constructor(width,height){
-        super();
-        this.width = width;
-        this.height = height;
+const resultAdd = operation.addNumbers(20, 30);
+const resultMultiply = operation.multiplyNumbers(3 , 6);
+console.log(resultAdd);
+console.log(resultMultiply);
+
+ 
+
+// Q4
+class Person { 
+    #name ;
+    #age; 
+
+    constructor(name, age ){
+        this.#name = name;
+        this.#age = age;
 
     }
-    calculatorArea(){
-        return this.width * this.height;
+    getName(){
+        return this.#name;
     }
+setName(name){
+    this.#name = name;
+}
+
+
+getAge (){
+    return this.#age;
+}
+setAge(age){
+    if(age  > 0 ){
+        this.#age = age;
+    }else{
+        console.log('please enter the valid  age');
+    }
+}
 
 }
-const circle = new Circle (6);
-console.log(`Area of circle : ${circle.calculatorArea()}`);
-const rectangle = new Rectangle(12 ,35);
-console.log(`Area of rectangle : ${rectangle.calculatorArea()}`)
+
+const person1 = new Person("john" , 35);
+console.log(person1.getName());
+console.log(person1.getAge());
+person1.setName ('Alice');
+person1.setAge(30);
+console.log(person1.getName());
+console.log(person1.getAge());
+
+// person1.setAge(-5);
+// console.log(person1.getAge());
